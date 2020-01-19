9eb10ae294891ec8bf094d3c74a9722024fea2e4
Murat Eserbekov <murat_eserbekov@mail.ru>
2020-01-19 23:29:33 +0200

    Lesson Chai

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ function mult(a, b){
 function dif(a, b){
     return a-b
 }
-module.exports = {sum, mult, dif};
+module.exports = {sum, mult, dif, concat, isAdult};
+// Homework
+function concat(x,y){
+    if(typeof(x) === 'string' && typeof(y) === 'string'|| typeof(x) === 'number' && typeof(y) === 'string'|| typeof(x) === 'string' && typeof(y) === 'number'){
+        return x+y;
+    } else return undefined;
+}
+
+function isAdult(age){
+    if(age >= 18 && age <= 65) return 'Adult';
+    else return 'Your age does not fill the bill';
+}
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,12 @@
-const assert = require("Assert");
-const {sum, mult, dif} = require('../index.js');
+const assert = require('assert');
+const {expect} = require('chai');
+
+const {sum, mult, dif, concat, isAdult} = require('../index.js');
 
 describe('function sum', () => {
     it('should function sum equals 6', () => {
         assert.equal(sum(2, 4), 6);
+        expect(sum(2, 4)).eq(6);
     });
 
     it('should function sum not equals 0', () => {
@@ -13,9 +16,42 @@ describe('function sum', () => {
 
 
 it('should function mult works correct', () => {
-    assert.equal(mult(2, 4), 8)
+    assert.equal(mult(2, 4), 8);
 });
 
 it('should function dif equals -2',() => {
     assert.equal(dif(2,4), -2)
+});
+
+// Homework
+describe('function concat',() => {
+    it('should correct concat number and string', () => {
+        assert.equal(concat(10, 'orange'), '10orange');
+    });
+
+    it('should correct concat string and string', () => {
+        assert.equal(concat('fruit', 'orange'), 'fruitorange');
+    });
+
+    it('should not correct concat number and number', () => {
+        assert.equal(concat(10, 20), undefined);
+    });
+
+    it('should not correct concat array and string', () => {
+        assert.equal(concat([], 'array'), undefined);
+    });
+
+    it('should not correct concat object and number', () => {
+        // assert.equal(concat({}, 1), undefined);
+        expect(concat({},1)).undefined;
+    });
+});
+
+describe('function isAdult',() => {
+    it('should person is Adult', () => {
+        assert.equal(isAdult(40), 'Adult')
+    });
+    it('should age does not fill the bill', () => {
+        assert.equal(isAdult(15), `Your age does not fill the bill`)
+    });
 });
