6f1d1ece5e1dc60595113205023262fbfcf9650f
Murat Eserbekov <murat_eserbekov@mail.ru>
2020-02-02 13:04:05 +0200

    Install ESlint

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 // Homework
 function sum(a, b){
-    return a + b;
+  return a + b;
 }
 function mult(a, b){
-    return a*b;
+  return a*b;
 }
 function dif(a, b){
-    return a-b;
+  return a-b;
 }
 // Функция склеивания
 function concat(x,y){
-    if(typeof(x) === 'string' && typeof(y) === 'string'|| typeof(x) === 'number' && typeof(y) === 'string'|| typeof(x) === 'string' && typeof(y) === 'number'){
-        return x+y;
-    } else return undefined;
+  if(typeof(x) === 'string' && typeof(y) === 'string'|| typeof(x) === 'number' && typeof(y) === 'string'|| typeof(x) === 'string' && typeof(y) === 'number'){
+    return x+y;
+  } else return undefined;
 }
 
 function isAdult(age){
-    if(age >= 18 && age <= 65) return 'Adult';
-    else return 'Your age does not fill the bill';
+  if(age >= 18 && age <= 65) return 'Adult';
+  else return 'Your age does not fill the bill';
 }
 // Функция сравнения переменных
 function comparison(a,b){
-    if(typeof(a) === 'string'||typeof(b) === 'string'){
-        a = +a;
-        b = +b;
-    }
-    return (a != b);
+  if(typeof(a) === 'string'||typeof(b) === 'string'){
+    a = +a;
+    b = +b;
+  }
+  return (a != b);
 }
 // Функция возведения в степень числа
 function power(num,pow){
-    if (num === 0) return null;
-    return (pow !== 0)? +(Math.pow(num,pow)).toFixed(2) : 1;
+  if (num === 0) return null;
+  return (pow !== 0)? +(Math.pow(num,pow)).toFixed(2) : 1;
 }
 
 // Функция нахождения четных чисел в массиве
 function arrayEvenNumber(arr){
-    const evenNumber = [];
-    for(let i = 0; i < arr.length; i++){
-      if(arr[i] % 2 === 0 && arr[i] > 0 && typeof(arr[i])==='number' ) {
-          evenNumber.push(arr[i]);
-      }
-    }return evenNumber;
+  const evenNumber = [];
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] % 2 === 0 && arr[i] > 0 && typeof(arr[i])==='number' ) {
+      evenNumber.push(arr[i]);
+    }
+  }return evenNumber;
 }
 
 module.exports = {sum, mult, dif, concat, isAdult, comparison, power,arrayEvenNumber};
